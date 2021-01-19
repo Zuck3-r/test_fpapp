4573de35b3daaa61e5f4f806dc934d5607007ac0
Zuck3-r <ns1122rx@gmail.com>
2021-01-19 09:13:58 +0000

    create planner user controller

--- app/controllers/planners_controller.rb
+++ app/controllers/planners_controller.rb
@@ -9,6 +9,19 @@ class PlannersController < ApplicationController
   end
   
   def create
+    @planner = Planner.new(planner_params)
     
+    if @planner.save
+      redirect_to root_url, success: "登録完了　ログインしてください"
+    else
+      render 'new'
+    end
   end
+
+
+  private
+  def planner_params
+    params.require(:planner).permit(:name, :email, :password, :password_confirmation)
+  end
+  
 end
