76ca3843df166c1a66b5b3e05b77806a46bb9835
Zuck3-r <ns1122rx@gmail.com>
2021-01-19 08:31:24 +0000

    add planner view

--- app/controllers/planners_controller.rb
+++ app/controllers/planners_controller.rb
@@ -1,4 +1,14 @@
 class PlannersController < ApplicationController
+  
+  def index
+    @planners = Planner.all
+  end
+  
   def new
+    @planner = Planner.new
+  end
+  
+  def create
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'planners/new'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'planners#index'
+  resources :planners
 end
