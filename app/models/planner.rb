8d73d482ff11f613729d93c2f4d591f896b9a718
Zuck3-r <ns1122rx@gmail.com>
2021-02-12 14:26:44 +0000

    add validation name

--- app/models/planner.rb
+++ app/models/planner.rb
@@ -5,10 +5,21 @@ class Planner < ApplicationRecord
   
   #保存する前に小文字化
   before_save { self.email = email.downcase }
+  validate :no_4_bytes
   validates :name, presence: true, length: { maximum: 40 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, {presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }, length: { maximum: 255 }}
   
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  
+    def no_4_bytes
+      if name.present?
+        chars = name.each_char.select{|c| c.bytes.count >= 4}
+        if chars.size > 0
+          errors.add(:name, "に絵文字(#{chars.join('')})は使用できません。")
+        end
+      end
+    end
+    
 end
