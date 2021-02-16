6578da99fc633c39d00a2fd47fbc50d639963fa3
Zuck3-r <ns1122rx@gmail.com>
2021-02-16 17:51:25 +0000

    fix-view's css

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,14 +1,15 @@
 class Customer < ApplicationRecord
   has_many :reservations
   
-  #それぞれのデータへのバリデーション
   before_save { self.email = email.downcase }
+  validate :no_4_bytes
   validates :name, presence: true, length: { maximum: 40 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, {presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }, length: { maximum: 255 }}
   
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  
     def no_4_bytes
       if name.present?
         chars = name.each_char.select{|c| c.bytes.count >= 4}
