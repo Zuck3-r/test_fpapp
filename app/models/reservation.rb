599cc553ba542976bb6bb04a1eaccc04b2eeca2b
Zuck3-r <ns1122rx@gmail.com>
2021-03-01 17:15:09 +0000

    fix resv validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,15 +5,34 @@ class Reservation < ApplicationRecord
   belongs_to :time_table
 
   validates :time_table_id, presence: true, inclusion: { in: 1..16 }
-  validates :date, presence: true, date: true
-  validates :planner_id, uniqueness: { scope: %i[time_table_id date] }
+  validates :date, presence: true
+  validates :planner_id, uniqueness: { scope: %i[time_table_id date], message: 'に同じ時間の予約枠が存在します' }, presence: true
   validates :customer_id, numericality: { only_integer: true }, allow_nil: true
+  validate :not_sunday
+  validate :not_before_today
   validate :saturday_time
+
+  START_TIME_ID = 3
+  END_TIME_ID = 10
+
+  scope :after_today, -> { where('date >= ?', Date.today) }
 
   def saturday_time
     return unless date.present?
     return unless time_table_id.present?
 
-    errors.add(:time_table_id, '時間外です') if date.saturday? && [*3..10].exclude?(time_table_id)
+    errors.add(:time_table_id, '範囲外です') if date.saturday? && [*START_TIME_ID..END_TIME_ID].exclude?(time_table_id)
+  end
+
+  def not_sunday
+    return unless date.present?
+
+    errors.add(:date, '日曜日は働いちゃダメです！') if date.sunday?
+  end
+
+  def not_before_today
+    return unless date.present?
+
+    errors.add(:date, '明日以降の日付を登録して下さい') if date <= Date.today
   end
 end
